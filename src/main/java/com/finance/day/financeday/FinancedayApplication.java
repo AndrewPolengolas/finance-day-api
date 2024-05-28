d5ef54482ad5462656e234dcdffb5cbcf53b9240
AndrewPolengolas <andrewnd2009@outlook.com>
2024-05-28 20:46:46 -0300

    Iniciando o Projeto Finance Day

---
+++ src/main/java/com/finance/day/financeday/FinancedayApplication.java
@@ -0,0 +1,13 @@
+package com.finance.day.financeday;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancedayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancedayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/finance/day/financeday/FinancedayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finance.day.financeday;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancedayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
